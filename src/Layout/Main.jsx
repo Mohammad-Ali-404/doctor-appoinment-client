e8155c514400ee32ba38d86e6329c593d3e78d77
wpmohammad1 <alimohammadjhd@gmail.com>
2023-12-26 15:37:27 +0600

    subscribe mail

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../Page/Shared/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Page/Shared/Footer/Footer';
+import SubscribeEmail from '../Page/Shared/SubscribeEmail/SubscribeEmail';
 
 const Main = () => {
     return (
         <div>
             <Navbar/>
             <Outlet/>
+            <SubscribeEmail/>
             <Footer/>
         </div>
     );
